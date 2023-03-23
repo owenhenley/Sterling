b74cf191f0352ba61df72616704b4f64c2ffe590
Owen Henley <owenhenley@me.com>
2023-03-23 17:01:53 +0000

    add base project

---
+++ Sterling/Sterling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sterling
+//
+//  Created by Owen Henley on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
